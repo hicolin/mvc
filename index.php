177d2cd2209cdb904b9ba666c6d6da8ba2fe37e9
Colin <811687790@qq.com>
2020-11-24 09:47:52 +0800

    Create index.php

--- index.php
+++ index.php
@@ -1,19 +1,24 @@
 <?php
 header('content-type:text/html;charset=utf-8');
 error_reporting(E_ALL &~E_NOTICE);
+
 require_once '/lib/common.func.php';
 require_once '/lib/mysql.inc.php';
+
 $pathinfo = substr($_SERVER['PATH_INFO'],1);
 $array = explode('/',$pathinfo);
 list($controller,$action) = $array;
+
 if($controller == ''){
     $controller = 'IndexController';
 }
 if($action == ''){
     $action = 'index';
 }
+
 //包含相关的控制文件
 require_once '/controller/' . $controller . '.php';
+
 //实例化控制器(路由过程)
 $class = new $controller();
 //调用方法
